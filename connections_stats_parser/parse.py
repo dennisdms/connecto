943115a5abdfb07378babff30f96c149c5f93f8e
dennisdms <schimtzekdennis@gmail.com>
2023-11-24 20:15:09 -0500

    init connection share string parsing

---
+++ connections_stats_parser/parse.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+from enum import Enum
+import re
+
+
+class Category(Enum):
+    YELLOW = '\U0001f7e8'
+    GREEN = '\U0001f7e9'
+    BLUE = '\U0001f7e6'
+    PURPLE = '\U0001f7ea'
+
+
+connections_pattern = r'Puzzle#(\d+)([🟦🟩🟪🟨]+)'
+
+
+class Connection:
+    def __init__(self, raw_guesses, puzzle_number, won, attempts, order):
+        self.raw_guesses = raw_guesses
+        self.puzzle_number = puzzle_number
+        self.won = won
+        self.attempts = attempts
+        self.order = order
+
+    def __eq__(self, other):
+        return self.raw_guesses == other.raw_guesses and \
+            self.puzzle_number == other.puzzle_number and \
+            self.won == other.won and \
+            self.attempts == other.attempts and \
+            self.order == other.order
+
+
+def parse_connection_share(share):
+    # Parses the connection share thingy and returns a Connection object
+    num, guesses = parse_connections(share)
+
+    order = []
+    for g in guesses:
+        guessed = is_correct_guess(g)
+        if guessed is not None:
+            order.append(guessed)
+
+    won = len(order) == 4
+
+    attempts = len(guesses)
+
+    return Connection(guesses, num, won, attempts, order)
+
+
+def parse_connections(connections):
+    # Parse a raw connection attempt and return puzzle_number, array of guesses
+    match = re.search(connections_pattern, remove_whitespace(connections))
+
+    if match:
+        puzzle_number = match.group(1)
+        emojis_group = match.group(2)
+
+        # Convert emojis group to an array
+        guesses = []
+        for i in range(0, len(emojis_group), 4):
+            guesses.append(list(emojis_group[i:i + 4]))
+
+        return int(puzzle_number), guesses
+
+
+def is_correct_guess(guess):
+    # Returns the color of the category guesses. If failed attempt return None
+    return guess[0] if guess[0] == guess[1] == guess[2] == guess[3] else None
+
+
+def remove_whitespace(s):
+    # Removes whitespace and newline chars from a string
+    return ''.join(s.split())
+
+
+if __name__ == '__main__':
+    simple_path = Path('../data', 'simple.txt')
+
+    puzzle_num, guesses = parse_connections(simple_path.read_text())
+
+    c = Connection(guesses, puzzle_num, )
+
+    c.puzzle_number = puzzle_num
---
+++ tests/test_parser.py
@@ -0,0 +1,81 @@
+import unittest
+from connections_stats_parser import parse
+
+
+class TestParse(unittest.TestCase):
+
+    def test_parse_connection_share(self):
+        input = """
+        Connections
+        Puzzle #166
+        🟪🟩🟩🟩
+        🟩🟩🟪🟩
+        🟦🟦🟦🟦
+        🟩🟩🟩🟩
+        🟨🟨🟪🟨
+        🟨🟨🟨🟨
+        🟪🟪🟪🟪
+        """
+        puzzle_num = 166
+        raw_guesses = [['🟪', '🟩', '🟩', '🟩'],
+                       ['🟩', '🟩', '🟪', '🟩'],
+                       ['🟦', '🟦', '🟦', '🟦'],
+                       ['🟩', '🟩', '🟩', '🟩'],
+                       ['🟨', '🟨', '🟪', '🟨'],
+                       ['🟨', '🟨', '🟨', '🟨'],
+                       ['🟪', '🟪', '🟪', '🟪']]
+        attempts = 7
+        won = True
+        order = ['🟦', '🟩', '🟨', '🟪']
+        expected = parse.Connection(raw_guesses, puzzle_num, won, attempts, order)
+        res = parse.parse_connection_share(input)
+        self.assertEquals(res, expected)
+
+    def test_parse_connections(self):
+        input = """
+        Connections
+        Puzzle #166
+        🟪🟩🟩🟩
+        🟩🟩🟪🟩
+        🟦🟦🟦🟦
+        🟩🟩🟩🟩
+        🟨🟨🟪🟨
+        🟨🟨🟨🟨
+        🟪🟪🟪🟪
+        """
+        expected = (166, [['🟪', '🟩', '🟩', '🟩'],
+                          ['🟩', '🟩', '🟪', '🟩'],
+                          ['🟦', '🟦', '🟦', '🟦'],
+                          ['🟩', '🟩', '🟩', '🟩'],
+                          ['🟨', '🟨', '🟪', '🟨'],
+                          ['🟨', '🟨', '🟨', '🟨'],
+                          ['🟪', '🟪', '🟪', '🟪']])
+        res = parse.parse_connections(input)
+        self.assertEquals(res, expected)
+
+    def test_is_correct_guess_1(self):
+        input = ['🟪', '🟪', '🟪', '🟪']
+        expected = '🟪'
+        res = parse.is_correct_guess(input)
+        self.assertEquals(res, expected)
+
+    def test_is_correct_guess_2(self):
+        input = ['🟪', '🟪', '🟪', '🟩']
+        res = parse.is_correct_guess(input)
+        self.assertIsNone(res)
+
+    def test_remove_special_chars(self):
+        input = """
+        Connections
+        Puzzle #166
+        🟪🟩🟩🟩
+        🟩🟩🟪🟩
+        🟦🟦🟦🟦
+        🟩🟩🟩🟩
+        🟨🟨🟪🟨
+        🟨🟨🟨🟨
+        🟪🟪🟪🟪
+        """
+        expected = """ConnectionsPuzzle#166🟪🟩🟩🟩🟩🟩🟪🟩🟦🟦🟦🟦🟩🟩🟩🟩🟨🟨🟪🟨🟨🟨🟨🟨🟪🟪🟪🟪"""
+        res = parse.remove_whitespace(input)
+        self.assertEquals(res, expected)
