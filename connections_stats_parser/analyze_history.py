59d85aea336fde677b15c3ff273b07c9c79886d5
dennisdms <schimtzekdennis@gmail.com>
2023-11-25 11:15:17 -0500

    function for analyzing a player's history

--- connections_stats_parser/analyze_history.py
+++ connections_stats_parser/analyze_history.py
@@ -1,9 +1,43 @@
+# Want to show:
+# 1. Total number of games played
+# 2. Number of games won
+# 3. Average number of tries (4 - 7) (attempt distribution)
+# 4. Guess matrix
+# 4.1 How many times have you guessed purple first?
+
+# TODO switch actual and expected
+# TODO Move this stuff into parse
+# TODO figure out better naming for all this stuff
+# TODO be able to parse a file where \r\n or \n is the seperator between attempts
+# TODO create ASCII Histogram for showing the attempt distribution and all the stats in general
+# TODO Connect to a discord channel
+# TODO create a dockerfile to run this and pass the API token and bot name as config
+# TODO run image on dwlabs server and connect it to squinner
+
 class ConnectionsStats:
-    def __init__(self):
-        self.attempts = None
-        self.wins = None
-        self.attempt_distribution = None
+    def __init__(self, attempts, wins, attempt_distribution, attempt_matrix):
+        self.attempts = attempts
+        self.wins = wins
+        self.attempt_distribution = attempt_distribution
+        self.attempt_matrix = attempt_matrix
+
+
+YELLOW = '\U0001f7e8'
+GREEN = '\U0001f7e9'
+BLUE = '\U0001f7e6'
+PURPLE = '\U0001f7ea'
 
 
 def analyze_history(connections_attempts):
-    pass
+    wins = 0
+    attempt_distribution = [0, 0, 0, 0]
+    attempt_matrix = {YELLOW: [0, 0, 0, 0], GREEN: [0, 0, 0, 0], BLUE: [0, 0, 0, 0], PURPLE: [0, 0, 0, 0]}
+    for attempt in connections_attempts:
+        if attempt.won:
+            wins += 1
+            attempt_distribution[attempt.attempts - 4] += 1
+            for i, category in enumerate(attempt.order):
+                attempt_matrix.get(category)[i] += 1
+
+    stats = ConnectionsStats(len(connections_attempts), wins, attempt_distribution, attempt_matrix)
+    return stats
--- tests/test_analyze_history.py
+++ tests/test_analyze_history.py
@@ -1,6 +1,21 @@
 import unittest
+from connections_stats_parser import parse, analyze_history
+from pathlib import Path
 
 
-class TestAnalyzeHistory(unittest):
-    def test(self):
-        pass
+class TestAnalyzeHistory(unittest.TestCase):
+    def test_analyze_history(self):
+        attempts = get_test_data('test1.txt', '***')
+        stats = analyze_history.analyze_history(attempts)
+        expected_attempt_matrix = {analyze_history.YELLOW: [2, 0, 2, 1],
+                                   analyze_history.GREEN: [1, 2, 0, 2],
+                                   analyze_history.BLUE: [2, 2, 1, 0],
+                                   analyze_history.PURPLE: [0, 1, 2, 2]}
+        self.assertEqual(stats.attempts, 6)
+        self.assertEqual(stats.wins, 5)
+        self.assertEqual(stats.attempt_distribution, [1, 0, 2, 2])
+        self.assertEqual(expected_attempt_matrix, stats.attempt_matrix)
+
+
+def get_test_data(file, seperator):
+    return parse.parse_file(Path('test_data', file), seperator)
