7205163fb532553dbfff1d3ea8de368f188e02fe
dennisdms <schimtzekdennis@gmail.com>
2023-11-25 16:12:47 -0500

    give types to classes and function signatures

--- connections_discord_bot/connections_parser.py
+++ connections_discord_bot/connections_parser.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import re
+from pathlib import Path
 
 
 # TODO give python3 types to everything
@@ -12,13 +13,13 @@ import re
 # Connections consists of four groups where each group is a category
 class ConnectionsResult:
     # A single connections result
-    def __init__(self, number, order, attempts, won):
-        self.puzzle_number = number
-        self.group_order = order
+    def __init__(self, puzzle_number: int, group_order: list[str], attempts: int, won: int) -> None:
+        self.puzzle_number = puzzle_number
+        self.group_order = group_order
         self.attempts = attempts
         self.won = won
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.puzzle_number == other.puzzle_number and \
             self.won == other.won and \
             self.attempts == other.attempts and \
@@ -27,7 +28,7 @@ class ConnectionsResult:
 
 class ConnectionsStats:
     # Aggregated statistics for a series of ConnectionsResults
-    def __init__(self, attempts, wins, attempt_distribution, attempt_matrix):
+    def __init__(self, attempts: int, wins: int, attempt_distribution: list[int], attempt_matrix: dict[str, list[int]]):
         self.attempts = attempts
         self.wins = wins
         self.attempt_distribution = attempt_distribution
@@ -40,7 +41,7 @@ BLUE = '\U0001f7e6'
 PURPLE = '\U0001f7ea'
 
 
-def analyze_connections_history(connections_attempts):
+def analyze_connections_history(connections_attempts: list[ConnectionsResult]) -> ConnectionsStats:
     wins = 0
     attempt_distribution = [0, 0, 0, 0]
     attempt_matrix = {YELLOW: [0, 0, 0, 0],
@@ -57,7 +58,7 @@ def analyze_connections_history(connections_attempts):
     return ConnectionsStats(len(connections_attempts), wins, attempt_distribution, attempt_matrix)
 
 
-def parse_connections_result(result):
+def parse_connections_result(result: str) -> ConnectionsResult:
     # Parses a connections result into a ConnectionsResult object
     num, guesses = parse_connections_share_string(result)
     order = []
@@ -73,7 +74,7 @@ def parse_connections_result(result):
 connections_pattern = r'Puzzle#(\d+)([🟦🟩🟪🟨]+)'
 
 
-def parse_connections_share_string(connections):
+def parse_connections_share_string(connections: str) -> tuple[int, list[list[str]]]:
     # Parse a raw connection attempt and return puzzle_number, array of guesses
     match = re.search(connections_pattern, remove_whitespace(connections))
     if match:
@@ -86,17 +87,17 @@ def parse_connections_share_string(connections):
         return int(puzzle_number), guesses
 
 
-def grouped_category(guess):
-    # Returns the color of the category correctly group. If failed return None
+def grouped_category(guess: list[str]) -> str:
+    # Returns the category correctly grouped. If failed, return None
     return guess[0] if guess[0] == guess[1] == guess[2] == guess[3] else None
 
 
-def parse_file(file, seperator):
+def parse_file(file: Path, seperator: str) -> list[ConnectionsResult]:
     # Parses a file of connections results - used primarily for testing
     attempts = file.read_text().split(seperator)
     return [parse_connections_result(i) for i in attempts]
 
 
-def remove_whitespace(s):
+def remove_whitespace(s: str) -> str:
     # Removes whitespace and newline chars from a string
     return ''.join(s.split())
